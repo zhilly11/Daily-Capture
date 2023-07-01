5351817e28706e829f9f6e74652157bd781ba4fe
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-07-01 20:20:12 +0900

    feat: MainViewModel 초기구현

--- Daily-Capture/Daily-Capture/Source/MainView/MainViewModel.swift
+++ Daily-Capture/Daily-Capture/Source/MainView/MainViewModel.swift
@@ -2,7 +2,18 @@
 //  Created by zhilly, vetto on 2023/06/30
 
 import Foundation
+import RxSwift
 
-class MainViewModel {
+final class MainViewModel {
+    let diaryList: BehaviorSubject<[Diary]> = .init(value: [])
+    private let disposeBag: DisposeBag = .init()
     
+    init(nowDate: Date) {
+        setupDiary(date: nowDate)
+    }
+    
+    // TODO: CoreData에서 날씨로 검색해서 데이터 가져온 후에 List에 넣어주기
+    func setupDiary(date createAt: Date?) {
+        print("setup Diary!")
+    }
 }
