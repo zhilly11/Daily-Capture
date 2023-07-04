d8b3e11a414a89f3c1335f92d941f81682d0701b
vetto <gzzjk159@naver.com>
2023-07-04 16:34:07 +0900

    feat: stackView 구현

---
+++ Daily-Capture/Daily-Capture/Source/DiaryDetailViewModel.swift
@@ -0,0 +1,12 @@
+//  Daily-Capture - DiaryDetailViewModel.swift
+//  Created by zhilly, vetto on 2023/06/30
+
+import Foundation
+
+final class DiaryDetailViewModel {
+    var diary: Diary?
+    
+    init(diary: Diary) {
+        self.diary = diary
+    }
+}
--- Daily-Capture/Daily-Capture/Source/MainView/ViewController.swift
+++ Daily-Capture/Daily-Capture/Source/MainView/ViewController.swift
@@ -4,8 +4,39 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    let button: UIButton = {
+        let button: UIButton = .init(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("다음", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(moveDiaryDetailView), for: .touchUpInside)
+        
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        [button].forEach(view.addSubview(_:))
+        configureButton()
+    }
+    
+    @objc
+    private func moveDiaryDetailView() {
+        let viewModel = DiaryDetailViewModel(diary: Diary(pictures: [], title: "hi", createdAt: Date()))
+        let diaryDetailView = UINavigationController(rootViewController: DiaryDetailViewController(diaryDetailViewModel: viewModel))
+        diaryDetailView.modalPresentationStyle = .fullScreen
+        
+        self.present(diaryDetailView, animated: true)
+    }
+    
+    private func configureButton() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor)
+        ])
     }
 }
