ac90acd935e0905e6dce98edbd85fbf5f54e0ff6
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-07-06 00:34:04 +0900

    feat: Date Extentions 구현
    - 실행 기기의 현재 시간을 가녀오는 메서드 구현
    - 일정 날짜를 기준으로 시작과 끝의 Date를 구하는 메서드 구현

---
+++ Daily-Capture/Daily-Capture/Source/Util/Extensions/Date+.swift
@@ -0,0 +1,32 @@
+//  Daily-Capture - Date+.swift
+//  Created by zhilly, vetto on 2023/07/05
+
+import Foundation
+
+extension Date {
+    static func localizedDate() -> Date {
+        let today: Date = .init()
+        let timezone: TimeZone = TimeZone.autoupdatingCurrent
+        let secondsFromGMT: Int = timezone.secondsFromGMT(for: today)
+        let currentDate: Date = today.addingTimeInterval(TimeInterval(secondsFromGMT))
+        
+        return currentDate
+    }
+    
+    func startOfDay() -> Date {
+        let calendar: Calendar = Calendar.current
+    
+        return calendar.startOfDay(for: self)
+    }
+    
+    func endOfDay() -> Date {
+        let calendar: Calendar = Calendar.current
+        var components: DateComponents = .init()
+        
+        components.day = 1
+        components.second = -1
+        
+        let endOfDay: Date? = calendar.date(byAdding: components, to: startOfDay())
+        return endOfDay ?? self
+    }
+}
