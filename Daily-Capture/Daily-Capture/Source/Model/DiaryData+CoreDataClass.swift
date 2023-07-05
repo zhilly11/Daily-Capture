b8924414301fd8574001a5d5a77d2f3cf7df7361
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-07-05 15:32:40 +0900

    feat: CoreDataModel 생성

---
+++ Daily-Capture/Daily-Capture/Source/Model/DiaryData+CoreDataClass.swift
@@ -0,0 +1,11 @@
+//   - DiaryData+CoreDataClass.swift
+//  Created by zhilly on 2023/07/05
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryData)
+public class DiaryData: NSManagedObject {
+
+}
---
+++ Daily-Capture/Daily-Capture/Source/Model/DiaryData+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//   - DiaryData+CoreDataProperties.swift
+//  Created by zhilly on 2023/07/05
+//
+
+import Foundation
+import CoreData
+
+
+extension DiaryData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<DiaryData> {
+        return NSFetchRequest<DiaryData>(entityName: "DiaryData")
+    }
+
+    @NSManaged public var content: String?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var pictures: Data?
+    @NSManaged public var title: String?
+    @NSManaged public var weather: Data?
+
+}
