b7d50a109163e6521aeff43cc4cd46fc4c369a27
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-08-24 16:43:46 +0900

    feat: 날씨 이미지 한글 설명 추가

--- Daily-Capture/Daily-Capture/Source/Util/Constant.swift
+++ Daily-Capture/Daily-Capture/Source/Util/Constant.swift
@@ -4,16 +4,33 @@
 import Foundation
 
 struct Constant {
-    static let weatherNameList: [String] = ["clear",
-                                            "cloudy-rainny",
-                                            "cloudy-sunny",
-                                            "cloudy",
-                                            "fullmoon",
-                                            "heavyrain-storm",
-                                            "night-rain",
-                                            "night",
-                                            "night-clear",
-                                            "rain",
-                                            "sunny",
-                                            "thunder"]
+    static let weatherNameList: [String] = [
+        "clear",
+        "cloudy-rainny",
+        "cloudy-sunny",
+        "cloudy",
+        "fullmoon",
+        "heavyrain-storm",
+        "night-rain",
+        "night",
+        "night-clear",
+        "rain",
+        "sunny",
+        "thunder"
+    ]
+    
+    static let weatherDescription: [String: String] = [
+        "clear": "맑음",
+        "cloudy-rainny": "흐림 비",
+        "cloudy-sunny": "흐림 해",
+        "cloudy": "흐림",
+        "fullmoon": "보름달",
+        "heavyrain-storm": "천둥 번개",
+        "night-rain": "밤에 비",
+        "night": "밤",
+        "night-clear": "맑은 밤",
+        "rain": "비",
+        "sunny": "해 쨍쨍",
+        "thunder": "천둥"
+    ]
 }
