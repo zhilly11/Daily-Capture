cd5d7950a528b354db19a8dd73196cd0a622d2db
vetto <gzzjk159@naver.com>
2023-09-01 05:11:54 +0900

    refactor: 사용하지 않은 코드 주석처리

--- Daily-Capture/Daily-Capture/Source/View/EditTableView/SubView/WeatherPickerViewModel.swift
+++ Daily-Capture/Daily-Capture/Source/View/EditTableView/SubView/WeatherPickerViewModel.swift
@@ -1,27 +1,27 @@
-//  Daily-Capture - WeatherPickerViewModel.swift
-//  Created by zhilly, vetto on 2023/07/28
-
-import RxSwift
-
-final class WeatherPickerViewModel {
-    // MARK: - Properties
-
-    let weatherText: BehaviorSubject<String> = .init(value: "clear")
-    var weatherImage: UIImage? {
-        get {
-            return UIImage(named: getWeatherString())
-        }
-    }
-    
-    // MARK: - Methods
-
-    private func getWeatherString() -> String {
-        do {
-            return try weatherText.value()
-        } catch {
-            print("error")
-        }
-        
-        return ""
-    }
-}
+////  Daily-Capture - WeatherPickerViewModel.swift
+////  Created by zhilly, vetto on 2023/07/28
+//
+//import RxSwift
+//
+//final class WeatherPickerViewModel {
+//    // MARK: - Properties
+//
+//    let weatherText: BehaviorSubject<String> = .init(value: "clear")
+//    var weatherImage: UIImage? {
+//        get {
+//            return UIImage(named: getWeatherString())
+//        }
+//    }
+//
+//    // MARK: - Methods
+//
+//    private func getWeatherString() -> String {
+//        do {
+//            return try weatherText.value()
+//        } catch {
+//            print("error")
+//        }
+//
+//        return ""
+//    }
+//}
